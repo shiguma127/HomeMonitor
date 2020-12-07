d392b9c5e82ff48887caa89cc7fad45e66cacd21
shiguma127 <shiguma127@gmail.com>
2020-12-07 14:56:06 +0900

    BarChart from config.json

--- pages/components/BarCard.tsx
+++ pages/components/BarCard.tsx
@@ -1,45 +1,25 @@
 import {Bar} from "react-chartjs-2";
-import React, {useState} from "react";
-import {render} from "next/client";
-import {Button, Card, CardContent, Grid, Typography} from "@material-ui/core";
+import React from "react";
+import {Card, CardContent, Typography} from "@material-ui/core";
 import styles from "../../styles/Home.module.css";
-import Icon from "@mdi/react";
-import {mdiWater} from "@mdi/js";
 
-const BarCard = (props)=> {
-    const [data,setData] :any= useState(props.data);
-    const [cnt,setCnt] = useState();
-    const updateData = ()=>{
-        let cp :any = {}
-        Object.assign(cp,data)
-        cp.datasets[0].data[0]++
-        setData(cp)
-    }
+const BarCard = (props) => {
     return (
         <div>
-            <Card variant={"outlined"} className={styles.fullCard}>
+            <Card variant={"outlined"} className={styles.fullCard} >
                 <CardContent>
                     <Typography variant={"h6"}>
-                        <Icon path={mdiWater}
-                              size={1}
-                        />
-                        Humidity
+                        {props.data.datasets[0].label}
                     </Typography>
-                    <Typography variant={"h4"} align={"center"}>
-                        {cnt}%
+                    <Typography variant={"h3"} align={"center"}>
+                        {props.data.datasets[0].data[0]}{props.unit}
                     </Typography>
-                    <Typography variant={"h4"} align={"center"}>
-                        {props.data.datasets[0].data[0]}%
-                    </Typography>
+                    <div>
+                        <Bar data={props.data} options={props.options} height={300} width={150} redraw/>
+                    </div>
                 </CardContent>
-                <Bar data={props.data} options={props.options} height={150} width={60} redraw/>
             </Card>
-            <Button variant="contained" onClick={updateData} >+</Button>
-            <button onClick={()=>{console.log(data)}}>
-                Click me
-            </button>
         </div>
     );
 }
-
 export default BarCard
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,143 +1,75 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import {AppBar, Button, Card, CardContent, Grid, IconButton, Toolbar, Typography} from "@material-ui/core";
+import {AppBar, Button, Grid, IconButton, Toolbar, Typography} from "@material-ui/core";
 import Icon from "@mdi/react";
-import {mdiThermometer, mdiWater} from '@mdi/js';
-import {Bar} from "react-chartjs-2";
-import {display} from "@material-ui/system";
+import {mdiThermometer} from '@mdi/js';
 import React from "react";
 import BarCard from "./components/BarCard";
+import BarData from "../src/BarData";
 
-const func = function () {
+let configs = []
+const datas = []
 
-}
-const data2 = {
-    labels: ['aaa'],
-    datasets: [
-        {
-            label:'aaa',
-            data: [59],
-            barThickness: 35,
-            backgroundColor: [
-                'rgb(99,224,255)'
-            ],
-            borderColor: [
-                'rgba(99,224,255 1)'
-            ],
-            borderWidth: 0,
-            borderSkipped:'end',
-        },
-    ],
-}
+class Home extends React.Component<any, any> {
+    constructor(props) {
+        super(props);
+        configs = require('../config/config.json').configs
+        configs.forEach(config => {
+            datas.push(new BarData(config.label, config.color, config.unit, config.range.min, config.range.max))
+        })
+        console.log(datas)
+        this.state = {datas: datas}
+    }
 
-const data = {
-    labels: ['aaa'],
-    datasets: [
-        {
-            label:'aaa',
-            data: [100],
-            barThickness: 35,
-            backgroundColor: [
-                'rgba(255, 99, 132, 255)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)'
-            ],
-            borderWidth: 0,
-            borderSkipped:'end',
-        },
-    ],
-}
-const options = {
-    maintainAspectRatio: true,
-    responsive: true,
-    animation:false,
-    legend:{
-        display:false,
-    },
-    scales: {
-        xAxes:[
-            {
-                ticks: {
-                    display:false,
-                }
-            }
-        ],
-        yAxes: [
-            {
-                gridLines: {
-                    display:false
-                },
-                ticks: {
-                    display: true,
-                    max: 100,
-                    beginAtZero: true,
-                },
-            },
-        ],
-    },
-}
+    render() {
+        return (
+            <div className={styles.container}>
+                <Head>
+                    <title>HomeMonitor</title>
+                    <link rel="icon" href="/favicon.ico"/>
+                </Head>
+                <main className={styles.main}>
+                    <AppBar position="fixed">
+                        <Toolbar>
+                            <IconButton
+                                color="inherit"
+                                aria-label="open drawer"
+                                edge="start"
+                            >
+                                <Icon path={mdiThermometer}
+                                      size={1}
+                                />
 
-
-export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>HomeMonitor</title>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
-            <main className={styles.main}>
-                <AppBar position="fixed">
-                    <Toolbar>
-                        <IconButton
-                            color="inherit"
-                            aria-label="open drawer"
-                            edge="start"
-                        >
-                            <Icon path={mdiThermometer}
-                                  size={1}
-                            />
-
-                            <Typography variant="h6" noWrap>
-                                HomeMonitor
-                            </Typography>
-                        </IconButton>
-                    </Toolbar>
-                </AppBar>
-                <Grid container spacing={4}>
-                    <Grid item>
-                        <Card variant={"outlined"} className={styles.fullCard}>
-                            <CardContent>
-                                <Typography variant={"h6"}>
-                                    <Icon path={mdiWater}
-                                          size={1}
-                                    />
-                                    Humidity
+                                <Typography variant="h6" noWrap>
+                                    HomeMonitor
                                 </Typography>
-                                <Typography variant={"h4"} align={"center"}>
-                                    59%
-                                </Typography>
-                                <Bar data={data2} options={options} height={150} width={60} redraw={true}/>
-                            </CardContent>
-                        </Card>
+                            </IconButton>
+                        </Toolbar>
+                    </AppBar>
+                    <Grid container spacing={4}>
+                        {datas.map(d => {
+                            return (
+                                <Grid item>
+                                    <BarCard data={d.data} options={d.options} unit={d.unit}> </BarCard>
+                                </Grid>
+                            )
+                        })}
                     </Grid>
-                    <Grid item>
-                        <BarCard data={data2} options={options}> </BarCard>
-                    </Grid>
-                </Grid>
-                <Button variant="contained" onClick={function() { data2.datasets[0].data[0]+=1}}>Default</Button>
-            </main>
+                </main>
 
-            <footer className={styles.footer}>
-                <a
-                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Powered by{'         '}
-                    <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo}/>
-                </a>
-            </footer>
-        </div>
-    );
+                <footer className={styles.footer}>
+                    <a
+                        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Powered by{'         '}
+                        <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo}/>
+                    </a>
+                </footer>
+            </div>
+        );
+    }
 }
+
+export default Home
